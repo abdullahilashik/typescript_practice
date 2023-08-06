13a71b377f3858d78842425c1af35b77e330e380
Abdullahil Ashik Md. Arefin <abdullah.ashik.arefin@gmail.com>
2023-08-06 06:02:17 +0600

    utility classes

--- build/js/utility.js
+++ build/js/utility.js
@@ -5,16 +5,33 @@ var Gender;
 (function (Gender) {
     Gender[Gender["MALE"] = 0] = "MALE";
     Gender[Gender["FEMALE"] = 1] = "FEMALE";
-    Gender[Gender["UNAVAILABLE"] = 2] = "UNAVAILABLE";
+    Gender[Gender["NOTSET"] = 2] = "NOTSET";
 })(Gender || (Gender = {}));
 const p1 = {
     name: 'ashik',
     age: 30,
-    gender: Gender.UNAVAILABLE
-};
-const updatePersons = (person, propsToUpdate) => {
-    return Object.assign(Object.assign({}, person), propsToUpdate);
+    gender: Gender.NOTSET
 };
 console.log(p1);
-const p2 = updatePersons(p1, { gender: Gender.MALE });
+const updatePerson = (person, propsToUpdate) => {
+    return Object.assign(Object.assign({}, person), propsToUpdate);
+};
+const p2 = updatePerson(p1, { gender: Gender.MALE });
 console.log(p2);
+const gradingSystem = {
+    ashik: 'a',
+    arefin: 'b',
+    abdullah: 'a'
+};
+const gradeData = {
+    ashik: { assign1: 1, assign2: 90 },
+    arefin: { assign2: 2, assign1: 4 },
+    abdullah: { assign1: 1, assign2: 99 }
+};
+// pick and ommit
+const picked = {
+    assign1: 90
+};
+const ommited = {
+    assign2: 90
+};
--- src/utility.ts
+++ src/utility.ts
@@ -5,27 +5,56 @@ console.log('----------- UTILITY TYPES -----------');
 enum Gender {
     MALE,
     FEMALE,
-    UNAVAILABLE
+    NOTSET
 }
 
 interface Person {
     name: string,
     age: number,
-    gender: Gender
+    gender?: Gender
 }
 
-const p1 : Person  = {
+const p1 : Person = {
     name: 'ashik',
     age: 30,
-    gender: Gender.UNAVAILABLE
-}
-
-const updatePersons = (person: Person, propsToUpdate: Partial<Person>) : Person => {
-    return {...person, ...propsToUpdate};
-}
-
+    gender: Gender.NOTSET
+};
 
 console.log(p1);
 
-const p2: Person = updatePersons(p1, {gender: Gender.MALE})
+const updatePerson = (person: Person, propsToUpdate: Partial<Person>): Person => {
+    return {...person, ...propsToUpdate};
+};
+
+const p2= updatePerson(p1, {gender:Gender.MALE});
 console.log(p2);
+
+// record
+type names = 'ashik'| 'arefin' | 'abdullah'
+type grades = 'a' | 'b'
+const gradingSystem : Record<names, grades> = {
+    ashik: 'a',
+    arefin: 'b',
+    abdullah: 'a'
+};
+
+interface Grades {
+    assign1: number,
+    assign2: number
+}
+
+const gradeData : Record<names, Grades> = {
+    ashik: {assign1: 1, assign2: 90},
+    arefin: {assign2: 2, assign1: 4},
+    abdullah: {assign1: 1, assign2: 99}
+}
+
+// pick and ommit
+
+const picked : Pick<Grades, 'assign1'> = {
+    assign1: 90
+}
+
+const ommited : Omit<Grades, 'assign1'> = {
+    assign2: 90
+};
